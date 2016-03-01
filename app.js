6f8340375003f29cd217fa686e5f8fb518253135
John Skilbeck <john.skilbeck@gmail.com>
2016-02-29 17:32:42 -0800

    nightly

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ var fs = require('fs')
 	cheerio = require('cheerio'),
 	phantom = require('phantom'),
 	url = 'https://www.omnipaygroup.com/ramtool'
+	env = require('./lib/config/pass.js')
 	;
 
-phantom.create(function(ph){
-	return ph.createPage(function(page){
-		return page.open(url, function(status){
-			return page.evaluate(function(){
-				console.log(document);
-			});
-		});
-	});
+// phantom.create(function(ph){
+// 	return ph.createPage(function(page){
+// 		return page.open(url, function(status){
+// 			return page.evaluate(function(){
+// 				console.log(document);
+// 			});
+// 		});
+// 	});
+// });
+
+
+request.post({
+	headers: {'content-type': 'application/x-www-form-urlencoded'},
+	url: url,
+	body: "69="+env.user+"&76="+env.pass_html+"cmd=WELCOME&nextpage=login"
+	}, function(err,res,html){
+		// console.log(html);
+	if(!err){
+		var $ = cheerio.load(html);
+		console.log(html);
+	}
 });
 
 
-
-
-// request(url, function(err,res,html){
-// 	if(!err){
-// 		var $ = cheerio.load(html);
-// 		console.log(html);
-// 	}
-// })
